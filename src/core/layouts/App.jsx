f8b05cff9acd7590aa7d7c621abcf41ba5bc62c0
Marcel Joshua <marceljoshua69@gmail.com>
2023-08-29 13:35:26 +0700

    Uploading step 1

---
+++ src/core/layouts/App.jsx
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router'
+import Meta from '../components/Meta'
+import Navbar from '../components/Navbar'
+
+const hideNavbarPages = ['/success', '/login']
+
+export default function AppLayout({ children }) {
+  const router = useRouter()
+  const hideNavbar = hideNavbarPages.includes(router.asPath)
+
+  return (
+    <>
+      <Meta />
+      {hideNavbar ? null : <Navbar />}
+      {children}
+    </>
+  )
+}
---
+++ src/products/components/Card.jsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+
+export default function ProductCard({ product }) {
+  return (
+    <article className='product-card'>
+      <Link href={`/products/${product.slug}`}>
+        <img src={`/assets/${product.slug}.png`} />
+      </Link>
+
+      <header>
+        <p>{product.name}</p>
+      </header>
+
+      <footer>
+        <Link href={`/products/${product.slug}`} className='more'>
+          See More
+        </Link>
+
+        <div>
+          <span className='pill'>
+            {product.category}
+          </span>
+        </div>
+      </footer>
+    </article>
+  )
+}
